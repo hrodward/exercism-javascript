3c5086a30c2802ee0aefaf61d6a565ec17802e4c
Eduardo Garcia-Castrillon <edugct@gmail.com>
2019-11-05 14:10:00 +0100

    updated after feedback

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,6 +1,6 @@
 const ALPHABET_LENGTH = 'abcdefghijklmnopqrstuvwxyz'.length;
 
 export const isPangram = (input) => {
-  const onlyLetters = input.trim().toLowerCase().split('').filter(x => x.match(/[a-z]/));
+  const onlyLetters = input.trim().toLowerCase().match(/[a-z]/g);
   return new Set(onlyLetters).size == ALPHABET_LENGTH;
 };
