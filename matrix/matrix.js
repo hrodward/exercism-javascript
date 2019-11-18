95ffeca22eea9f7737fbb5e49748ec54fd873baa
Eduardo Garcia-Castrillon <edugct@gmail.com>
2019-11-18 14:13:14 +0100

    updated matrix after feedback

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,27 +1,29 @@
 export class Matrix {
 
   constructor(input) {
-    this.matrix = [];
-    input.split('\n').forEach(textRow => this.matrix.push(textRow.split(' ').map(Number)));
-    this.colsArray = [];
-    this.columns = this.colsArray;
+    this.matrix = input.split('\n').map(row => row.split(' ').map(Number));
   }
 
   get rows() {
     return this.matrix;
   }
 
-  set columns(arr) {
+  get columns() {
+    if (!this._columns) {
+      this._columns = this.calculateColumns();
+    }
+    return this._columns;
+  }
+
+  calculateColumns() {
+    let colArray = [];
     for (let i = 0; i < this.matrix[0].length; i++) {
       let col = [];
       for (let j = 0; j < this.matrix.length; j++) {
         col.push(this.matrix[j][i]);
       }
-      arr.push(col);
+      colArray.push(col);
     }
-  }
-
-  get columns() {
-    return this.colsArray;
+    return colArray;
   }
 }
